b9261c84821c402f2bb7d3b703ae8eb898161a9f
Phill Price <phill@csl-web.com>
2015-02-05 10:55:07 +0000

    moved routes to config.yml

--- _config.php
+++ _config.php
@@ -3,6 +3,3 @@
 // $mailer = new MandrillMailer('APIKEYHERE');
 // $mailer->set_track_opens(true);
 // $mailer->set_track_clicks(true);
-
-Director::addRules(50, array('mandrillevent//$Action/$ID/$Name'=> 'MandrillEvent_Controller'));
-
